1e9d42e7caf9564afd823399a7c77671dcf9881e
erase1657 <erase1657@naver.com>
2025-04-26 21:46:47 +0900

    가입된 정보 기준 로그인 기능 구현

--- app/src/main/java/com/example/alramapp/Authentication/LoginActivity.java
+++ app/src/main/java/com/example/alramapp/Authentication/LoginActivity.java
@@ -15,31 +15,53 @@ package com.example.alramapp.Authentication;
  * limitations under the License.
  */
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.credentials.GetCredentialRequest;
 
 import com.example.alramapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.BuildConfig;
 
 public class LoginActivity extends Activity {
-    private Button RegisterButton;
+    private Button GoRegister;
+    private Button LoginButton;
+    private Button Google, Github, Twitter, Facebook;
 
+    private EditText emailEditText;
+    private EditText passwordEditText;
     private static final String TAG = "EmailPassword";
-
     private FirebaseAuth mAuth; //Firebase 인증 객체 선언
 
     //인증 상태 확인
+    @SuppressLint("MissingInflatedId")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
 
-        RegisterButton = findViewById(R.id.registerbtn);
+        mAuth = FirebaseAuth.getInstance(); // Firebase 인증 객체 초기화
 
-        RegisterButton.setOnClickListener(new View.OnClickListener() {
+        GoRegister = findViewById(R.id.registerbtn);
+        LoginButton = findViewById(R.id.loginbtn);
+
+        emailEditText = findViewById(R.id.editemail);
+        passwordEditText = findViewById(R.id.editpassword);
+        GoRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
@@ -47,10 +69,32 @@ public class LoginActivity extends Activity {
             }
         });
 
-        mAuth = FirebaseAuth.getInstance(); // Firebase 인증 객체 초기화
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    return; // 입력이 없으면 더 이상 진행하지 않음
+                }
+
+                if (password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    return; // 입력이 없으면 더 이상 진행하지 않음
+                }
+
+                signIn(email,password);
+
+
+            }
+        });
     }
-    /*@Override
+
+
+    @Override
     public void onStart() {
         super.onStart();
 
@@ -62,22 +106,24 @@ public class LoginActivity extends Activity {
     }//인증 상태 확인 종료
 
 
-    //기존 사용자 로그인 메서드
+
+
+    //이메일 기반 인증
     private void signIn(String email, String password) {
-        // [START sign_in_with_email]
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(LoginActivity.this, "로그인 성공",
+                                    Toast.LENGTH_SHORT).show();
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
-                            // If sign in fails, display a message to the user.
+
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.makeText(LoginActivity.this, "이메일과 비밀번호를 다시 확인해주세요.",
                                     Toast.LENGTH_SHORT).show();
                             updateUI(null);
                         }
@@ -100,11 +146,24 @@ public class LoginActivity extends Activity {
         // [END send_email_verification]
     }
 
+
+/*
+    GetGoogleIdOption googleIdOption = new GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(true)
+            .setServerClientId(getString(R.string.default_web_client_id))
+            .build();
+
+    // Create the Credential Manager request
+    GetCredentialRequest request = new GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build();
+*/
+
     private void reload() {
 
     }
 
     private void updateUI(FirebaseUser user) {
 
-    }*/
+    }
 }
